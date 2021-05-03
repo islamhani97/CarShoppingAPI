259a9a94d6cf18cb23e5225e101c4533a5bdfc3b
Islam Hani <islamhani97>
2021-05-03 15:49:15 +0200

    Init project

---
+++ src/main/java/com/carshopping/CarShoppingApiApplication.java
@@ -0,0 +1,13 @@
+package com.carshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShoppingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShoppingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carshopping/CarShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShoppingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
